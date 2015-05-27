477504c2513e609d9249ea47013bf7babecc11c6
myb800 <myb800@gmail.com>
2015-05-26 20:27:12 -0700

    rdd2

--- src/main/scala/DataSource.scala
+++ src/main/scala/DataSource.scala
@@ -31,26 +31,10 @@ class DataSource(val dsp: DataSourceParams)
   def readTraining(sc: SparkContext): TrainingData = {
     val imageFile = Files.readAllBytes(Paths.get("/tmp/train-images-idx3-ubyte"))
     val labelFile = Files.readAllBytes(Paths.get("/tmp/train-labels-idx1-ubyte"))
-    var images = Seq[Array[Double]]()
-    var labels = Seq[Double]()
-    var a = 0
-    var b = 0
-    for(a <- 0 until 10000){
-      if(a % 1000 == 0){
-        logger.info("a " + a)
-      }
-      var image = Array[Double]()
-      for(b <- 0 until 28 * 28){
-        image = image :+ imageFile(a * 28 * 28 + b + 16).toDouble
-      }
-      images = images :+ image
-      labels = labels :+ labelFile(8 + a).toDouble
-    }
     val imageRDD = sc.parallelize(imageFile.toSeq).zipWithIndex.map{ case (p,i) => (i / (28 * 28),p.toDouble)}
                                                   .groupByKey.map{ case (i,p) => p.toArray}.cache
-    val labelRDD = sc.parallelize(labels.toSeq)
+    val labelRDD = sc.parallelize(labelFile.toSeq)
     new TrainingData(imageRDD.zip(labelRDD).map{ case (i,l) => Image(i,l.toDouble)})
-    new TrainingData(sc.parallelize(labels.zip(images)).map{ case (l,i) => Image(i,l) })
   }
 
 }
